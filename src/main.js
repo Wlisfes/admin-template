91a6ca481e42cf64f710bc1855de13c2f649d67d
情雨随风 <876451336@qq.com>
2020-07-13 21:46:46 +0800

    测试分支

--- src/main.js
+++ src/main.js
@@ -1,16 +1,18 @@
-import '@babel/polyfill';
+import '@babel/polyfill'
 
-import Vue from 'vue';
-import App from '@/App';
-import router from '@/router';
-import store from '@/store';
-import '@/core/use';
-import bootstrap from '@/core/bootstrap';
-import '@/permission';
+import Vue from 'vue'
+import App from '@/App'
+import router from '@/router'
+import store from '@/store'
+import '@/core/use'
+import bootstrap from '@/core/bootstrap'
+import '@/permission'
+
+console.log(process.env)
 
 new Vue({
 	router,
 	store,
 	created: bootstrap,
-	render: (h) => h(App)
-}).$mount('#app');
+	render: h => h(App)
+}).$mount('#app')
